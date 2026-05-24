863049a1c078206afe7a4a8a6b58e771ba0f5192
Ambroise52 <203607569+Ambroise52@users.noreply.github.com>
2026-05-24 16:13:28 +0300

    chore: add node_modules lock - app running successfully on localhost:3000

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-@'
 import React from "react";
 import ReactDOM from "react-dom/client";
 import DerivOracle from "./DerivOracle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<React.StrictMode><DerivOracle /></React.StrictMode>);
-'@ | Out-File -FilePath "src\index.js" -Encoding utf8
